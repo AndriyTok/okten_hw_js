52a0757351ea06258fa1f381ac9dedf2af55552e
andri <andriy.tokovyy@gmail.com>
2025-02-26 19:59:56 +0200

    hw 8 done

--- hw_8/main.js
+++ hw_8/main.js
@@ -2,12 +2,30 @@
 // Створити функцію, яка робить глибоку копію об'єкту.
 // Додати перевірки на undefined, null, NaN.
 //     Подумати і реалізувати логіку, в якій кінцевий об'єкт буде мати функції,які в нього були до цього моменту.
-function deepcopy (obj){
-    const copy = JSON.stringify(obj)
-    for (let key in copy){
-        key = copy?.key
+
+function deepcopy(obj){
+    if (obj){
+        const functions = []
+        for (let key in obj){
+            if (typeof(obj[key]) === 'function') {
+                const function_clone = obj[key].bind({})
+                functions.push({function_clone, key})
+            }
+        }
+        const clone = JSON.parse(JSON.stringify(obj))
+        for (const func of functions){
+            clone[func.key] = func.function_clone
+        }
+        for (let key in clone) {
+            if (clone[key] === null || clone[key] === undefined) {
+                delete clone[key];
+            }
+        }
+
+        return clone
+
     }
-    return JSON.parse(copy)
+
 }
 
 const user = {
@@ -15,11 +33,13 @@ const user = {
     surname: 'boyko',
     age:15,
     status: undefined,
+    greeting() {console.log(`Hello, ${user.name}`)},
 }
 
 
-console.log(deepcopy(user));
-
+const new_user = deepcopy(user)
+console.log(new_user);
+new_user.greeting()
 // #iz6emEsP2BA
 // - є масив
 const coursesAndDurationArray = [
