ffaba6d89c391906f8cc084a8f500707c64c69e3
andri <andriy.tokovyy@gmail.com>
2025-03-05 21:05:53 +0200

    hw 10 add 1 done moved main to main_dir

---
+++ hw_10/add_1/main.js
@@ -0,0 +1,26 @@
+// #bq1zkx7WP
+// *** (подібне було вище, але...будьте уважні в другій частині) створити сторінку з довільним блоком, в середині якого
+// є значення "100грн"
+// при перезавантаженні сторінки до значаення додається по 10грн, але !!!
+//     зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
+//     При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд - нічого не відбувається
+
+document.addEventListener('DOMContentLoaded', function () {
+    const number_div = document.getElementsByTagName('div')[0]
+
+    const last_time = JSON.parse(localStorage.getItem('last_time')) || 0;
+    const current_time = Date.now()
+
+    if (current_time - last_time >= 10000) {
+
+        localStorage.setItem('last_time', JSON.stringify(current_time));
+
+        let number = parseInt(localStorage.getItem('number')) || 100;
+        number = number + 10;
+        number_div.innerHTML = `${JSON.stringify(number)}грн`;
+        localStorage.setItem('number', JSON.stringify(number));
+
+    }
+})
+
+
